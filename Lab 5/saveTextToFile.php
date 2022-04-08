64c4ee41b42f91e6fcdd52b95bfb0dce15675dff
maister <s24306@pjwstk.edu.pl>
2022-04-08 10:27:30 +0200

    Finished task 2 from lab 5

--- Lab 5/saveTextToFile.php
+++ Lab 5/saveTextToFile.php
@@ -5,7 +5,6 @@ $text = $_POST['text'].'<br>';
 touch('./fileToWrite.txt');
 chmod('./fileToWrite.txt', 777);
 $f = file_put_contents('./fileToWrite.txt', $text, FILE_APPEND);
-//fwrite($f, $text) or die("write not working");
 
 echo file_get_contents('./fileToWrite.txt');
 echo "<br>";
